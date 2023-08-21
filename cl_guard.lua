17d74efca63790b600e0bc9f447f621755cfa612
Moch. Rifqi Alfaritsi <72482885+alfaritsii@users.noreply.github.com>
2023-08-22 06:32:46 +0700

    fix get banned when spamming TriggerServerEvent

--- cl_guard.lua
+++ cl_guard.lua
@@ -2,6 +2,7 @@ GuardServerEvent = TriggerServerEvent
 ATriggerServerEvent = TriggerServerEvent
 local Debug = DebugMode or false
 TriggerServerEvent = function(eventName, ...)
+    Wait(1000)
     return GuardServerEvent(eventName, LocalPlayer.state[GlobalState["fs-guard"]], ...)
 end
 
