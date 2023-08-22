4fe29ef2a4c8f7bfc7497b8cf4c1cd33c7ddf7bd
Moch. Rifqi Alfaritsi <72482885+alfaritsii@users.noreply.github.com>
2023-08-22 19:23:37 +0700

    fix delay for ox_lib callback

--- cl_guard.lua
+++ cl_guard.lua
@@ -2,10 +2,10 @@ GuardServerEvent = TriggerServerEvent
 ATriggerServerEvent = TriggerServerEvent
 local Debug = DebugMode or false
 TriggerServerEvent = function(eventName, ...)
-    Wait(1000)
     if string.find(eventName, "__ox_cb") then
         return ATriggerServerEvent(eventName, ...)
     else
+        Wait(2500)
         return GuardServerEvent(eventName, LocalPlayer.state[GlobalState["fs-guard"]], ...)
     end
 end
